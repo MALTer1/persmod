0b040bd69e5e67fcc885b343fc179acd914d57c4
MALTer1 <mancoon.help@gmail.com>
2026-09-24 00:24:47 +0000

    Initial Fabric mod setup

---
+++ src/client/java/com/eclipseashes/client/ExampleModClient.java
@@ -0,0 +1,11 @@
+package com.eclipseashes.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EclipseAshesClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
